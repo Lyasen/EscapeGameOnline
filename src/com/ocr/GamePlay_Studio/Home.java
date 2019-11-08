163556daf7b72a4285acd90f4d9966da63d459c1
Lyasen <rems.h51@hotmail.fr>
2019-11-08 21:19:57 +0100

    Implementing methods in Home class

--- src/com/ocr/GamePlay_Studio/Home.java
+++ src/com/ocr/GamePlay_Studio/Home.java
@@ -15,45 +15,47 @@ class Home {
                 "\n1- Challenger" +
                 "\n2- defender" +
                 "\n3- Duel");
-
         int response = 0;
-        boolean condition = false;  //  Condition in case of Exceptions
-
         /*
          * Choice of different game modes
          * As long as the answer isn't between 1 & 3, re-ask the question
          */
-        while (!condition) {
-            try {
-                do {
-                    response = scan.nextInt();
+        do {
+           response = userChoice();
+           printUserChoice(response);
+        } while (!(1 <= response && response <= 3));
+    }
 
-                    switch (response) {
-                        case 1:
-                            System.out.println("You have choice the game mode : Challenger");
-                            System.out.println("Find the secret number");
-                            break;
-                        case 2:
-                            System.out.println("You have choice the game mode : Defender");
-                            System.out.println("Will the computer regain your secret combination ?");
-                            break;
-                        case 3:
-                            System.out.println("You have choice the game mode : Duel");
-                            System.out.println("Challenge the computer");
-                            break;
-                        default:
-                            System.out.print("Your choice doesn't correspond to the choices offered, please try again : ");
-                    }
-                } while (!(1 <= response && response <= 3));
-                condition = true;
-            } catch (InputMismatchException e) {
-                System.out.print("Your choice must match a menu choice" +
-                        "\n1- Challenger" +
-                        "\n2- defender" +
-                        "\n3- Duel" +
-                        "\nPlease try again : ");
-                scan.nextLine();    //  dump the variable otherwise infinite loop
-            }
+    private int userChoice(){
+        try {
+            return scan.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.print("Your choice must match a menu choice" +
+                    "\n1- Challenger" +
+                    "\n2- defender" +
+                    "\n3- Duel" +
+                    "\nPlease try again : ");
+            scan.nextLine();    //  dump the variable otherwise infinite loop
+        }
+        return userChoice();
+    }
+
+    private void printUserChoice(int response){
+        switch (response) {
+            case 1:
+                System.out.println("You have choice the game mode : Challenger");
+                System.out.println("Find the secret number");
+                break;
+            case 2:
+                System.out.println("You have choice the game mode : Defender");
+                System.out.println("Will the computer regain your secret combination ?");
+                break;
+            case 3:
+                System.out.println("You have choice the game mode : Duel");
+                System.out.println("Challenge the computer");
+                break;
+            default:
+                System.out.print("Your choice doesn't correspond to the choices offered, please try again : ");
         }
     }
 }
