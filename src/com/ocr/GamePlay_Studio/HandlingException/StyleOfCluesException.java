a0f467f64f4a7eb1563db82bb5c5edf145eca0a5
Lyasen <rems.h51@hotmail.fr>
2020-01-08 16:43:08 +0100

    entirely erase project in order to create a POO project

--- src/com/ocr/GamePlay_Studio/HandlingException/StyleOfCluesException.java
+++
@@ -1,7 +0,0 @@
-package com.ocr.GamePlay_Studio.HandlingException;
-
-public class StyleOfCluesException extends Exception {
-    public StyleOfCluesException() {
-        System.out.println("What sort of clues is that ? Please enter only the real symbols !");
-    }
-}
