a155f3789405548d3642823790b41bf09beb0548
Lyasen <rems.h51@hotmail.fr>
2020-01-16 10:58:51 +0100

    Modification in mode duel

--- com.GamePlayStudio/gameMode/DuelMode.java
+++ com.GamePlayStudio/gameMode/DuelMode.java
@@ -27,44 +27,46 @@ public class DuelMode {
         System.out.println("Now, computer and human deliver a real fight !" +
                 "\nYou'll have " + config.getMaxTries() + " tries");
         int[] hazard = random.randomSecret(config);
+        int[] combinationPlayer;
+        int[] combinationAi;
         do {
             /*
              * Player's proposition
              */
-            int[] combinationPlayer = playerPlay.propositionPlayer(scan, config);
-            String[] comparePlayer = compare.compareDigits(combinationPlayer, hazard, config);
+            combinationPlayer = playerPlay.propositionPlayer(scan, config);
+            String[] compareCombinations = compare.compareDigits(combinationPlayer, hazard, config);
             if (counter > 1)
-                System.out.println("The clues are  : " + Arrays.toString(comparePlayer));
+                System.out.println("The clues are  : " + Arrays.toString(compareCombinations));
 
-            if (IsWin.winIf(comparePlayer)) {
+            if (IsWin.winIf(compareCombinations)) {
                 System.out.println("\nWell done ! HUMAN WIN !");
                 break;
             }
             counter--;
-            System.out.println("It stays " + counter + " tries");
+            System.out.printf("It stays %d tries", counter);
 
             /*
              * AI's proposition
              */
-            int[] combinationAi = aiPlay.AiSearch(combinationPlayer, comparePlayer, mini, maxi);
-            System.out.println("\nAI's proposition : " + Arrays.toString(combinationAi));
-            String[] compareAi = compare.compareDigits(hazard, combinationAi, config);
+            combinationAi = aiPlay.AiSearch(combinationPlayer, compareCombinations, mini, maxi);
+            System.out.println("\n\nAI's proposition : " + Arrays.toString(combinationAi));
+            compareCombinations = compare.compareDigits(combinationPlayer, hazard, config);
             if (counter > 1)
-                System.out.println("\rThe clues are  : " + Arrays.toString(compareAi));
+                System.out.println("\rThe clues are  : " + Arrays.toString(compareCombinations));
+            counter--;
+            System.out.printf("It stays %d tries", counter);
 
-            if (IsWin.winIf(compareAi)) {
+            if (IsWin.winIf(compareCombinations)) {
                 System.out.println("\nWell done ! AI win !");
                 break;
             }
 
-            if (counter == 0) {
-                System.out.println("Sorry, no more tries ! The secret number was : "
+            if (counter < 0) {
+                System.out.println("\nSorry, no more tries ! The secret number was : "
                         + Arrays.toString(combinationPlayer) + " ! Try next time !");
                 break;
-            } else {
-                System.out.printf("\nWell ! You'll have played one time each other, it stays %d tries", counter);
             }
-        } while (true);
+        } while (counter > 0 || Arrays.equals(combinationPlayer, hazard) || Arrays.equals(combinationAi, hazard));
     }
 
 }
